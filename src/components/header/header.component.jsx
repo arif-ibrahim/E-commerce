95000e9211eb6b2edca7960e38c653a30d12e448
Ibrahim Arif <ibrahimarif@Mohammads-iMac.local>
2020-03-15 12:26:08 +0600

    modified header file

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
 
-
+React.createElement()
 const Header = () => (
     <div className="header">
         <Link to='/' className="logo-container"><img src="https://a.udemycdn.com/2019-06-30_21-01-32-9996f50fd92611c5a1dc2be86a61ae44/original.svg?nva=20200314164659&token=06fe85b6a472f59344022" alt=""/></Link>
