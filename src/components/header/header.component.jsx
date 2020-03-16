4af8aa7e7bcdef7394aebfa3f4baa03761ad78bc
Ibrahim Arif <ibrahimarif@Mohammads-iMac.local>
2020-03-16 11:59:51 +0600

    add sign in module, form input and custom button

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './header.styles.scss';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 React.createElement()
 const Header = () => (
     <div className="header">
-        <Link to='/' className="logo-container"><img src="https://a.udemycdn.com/2019-06-30_21-01-32-9996f50fd92611c5a1dc2be86a61ae44/original.svg?nva=20200314164659&token=06fe85b6a472f59344022" alt=""/></Link>
+        <Link to='/' className="logo-container"><Logo/></Link>
         <div className="options">
             <Link to='/shop' className="option">Shop</Link>
             <Link to='#' className="option">Contact</Link>
