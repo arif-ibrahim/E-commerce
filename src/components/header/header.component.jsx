13551526014e43270b69064134772b8197510c56
arif87 <arif.ibrahm@gmail.com>
2020-08-20 01:16:51 +0600

    added cartIcon and cartDropdown component and also implemented cart reducers

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from '../../firebase/firebase.utils';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
  
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link to='/' className="logo-container"><Logo/></Link>
         <div className="options">
@@ -18,12 +20,17 @@ const Header = ({ currentUser }) => (
                 <div className="option" onClick={()=>auth.signOut()}>Sign Out</div>
                 : <Link to="signin"className="option">Sign In</Link>
             }
+            <CartIcon/>
         </div>
+        {hidden ? null : 
+        <CartDropdown/>}
+
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser,
+const mapStateToProps =  ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden,
 
 })
 
---
+++ src/redux/cart/cart.actions.js
@@ -0,0 +1,7 @@
+import CartActionTypes from './cart.types';
+import CartActionsTypes from './cart.types';
+
+
+export const toggleCartHidden = () => ({
+    type: CartActionsTypes.TOGGLE_CART_HIDDEN
+})
